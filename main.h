feaeaa1e3ce18c85a451f4ba61529f6f583aa12b
benromaissae1 <benromaissae1@gmai.com>
2023-08-23 15:16:12 -0700

    uploading

--- main.h
+++ main.h
@@ -4,7 +4,7 @@
 #include<stdio.h>
 #include<unistd.h>
 
-#define UNUSED(x)(void)(x)
+#define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
 /*THE FLAGS*/
@@ -62,8 +62,8 @@ int print_octal(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_hexadecimal(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-int print_hexa(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
+int print_hexa(va_list types, char map_to[],
+	char buffer[], int flags, char flag_ch, int width, int precision, int size);
 int print_hexa_upper(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
